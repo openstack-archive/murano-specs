dd11d1ccc0c4ac399d45873e070cd3b7975cecfe
Ekaterina Chernova <efedorova@mirantis.com>
2015-10-22 11:47:18 +0300

    Use default theme for building on RTD
    Change-Id: I156b94ad1364589098a2c73e4ae21f4e1eb048ce
    Partial-Bug: #1508712

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -99,13 +99,6 @@ man_pages = []
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-if on_rtd:
-  html_theme = 'sphinx_rtd_theme'
-else:
-  html_theme = 'nature'
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
